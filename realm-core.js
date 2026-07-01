af99cc2c95bffe9172111c9748d925ab7b8795d4
iki1uc <entertrainer@web.de>
2026-07-01 16:45:14 +0200

    Add Realm function with tick method

--- realm-core.js
+++ realm-core.js
@@ -1,1 +1,20 @@
+export function Realm(instanzen, ghost, mir, argumenteria, dir) {
+  return {
+    tick(impulseText) {
+      const impulse = mir(impulseText);
+      const ghostSense = ghost.sense(impulse);
+
+      const outputs = instanzen.map(i => {
+        const arg = argumenteria(i, impulse);
+        const out = dir(arg);
+        return { id: i.ROOT.ID, out };
+      });
+
+      return {
+        ghostSense,
+        outputs
+      };
+    }
+  };
+}
 

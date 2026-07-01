ca55aa16502d4099a99bcc3ffd696d482707784d
iki1uc <entertrainer@web.de>
2026-07-01 20:34:36 +0200

    Add functions to manage step states in visio-engine

--- visio-engine.js
+++ visio-engine.js
@@ -1,1 +1,15 @@
+export function visClear() {
+  document.querySelectorAll(".step").forEach(s => {
+    s.classList.remove("active");
+    s.classList.remove("fail");
+  });
+}
+
+export function visOn(id) {
+  document.getElementById(id).classList.add("active");
+}
+
+export function visFail(id) {
+  document.getElementById(id).classList.add("fail");
+}
 

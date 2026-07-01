36670cebe39c8edfc8b5f6036af583af2bd18cd4
iki1uc <entertrainer@web.de>
2026-07-01 16:12:09 +0200

    Update instanz-core.js

--- instanz-core.js
+++ instanz-core.js
@@ -1,3 +1,18 @@
 export function Instanz(ID, GATE, SAT, ANKER) {
-  return { ID, GATE, SAT, ANKER };
+  return {
+    ROOT: {
+      ID,
+      GATE,
+      SAT,
+      ANKER
+    },
+
+    NC: {
+      ghost: false,
+      wraight: false,
+      error: null,
+      help: null,
+      scalar: null
+    }
+  };
 }
